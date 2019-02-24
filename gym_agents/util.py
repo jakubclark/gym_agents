ae4c8bd035d39cff743a4746e1fa1071b666eebd
Kuba Clark <jakub.clark@protonmail.com>
2019-02-24 16:38:41 +0100

    fix flatten_shape function

--- gym_agents/util.py
+++ gym_agents/util.py
@@ -1,9 +1,8 @@
 from gym.spaces import Box
 
 
-def flatten_shape(observation_space):
-    if isinstance(observation_space, Box):
-        res = 1
-        for i in observation_space.shape:
-            res *= i
-        return res
+def flatten_shape(observation_space: Box):
+    res = 1
+    for i in observation_space.shape:
+        res *= i
+    return res
